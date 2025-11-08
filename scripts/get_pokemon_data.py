4b76c8337f73b9e414f8a716f557af75e32e0028
enrikina <enrikina98@gmail.com>
2025-11-07 20:04:49 -0500

    script complete

--- scripts/get_pokemon_data.py
+++ scripts/get_pokemon_data.py
@@ -4,10 +4,13 @@ from typing import Dict, Tuple, Optional
 import requests
 import re
 
-import os
+import yaml
+from tqdm import  tqdm
 
 from scripts.pokemon_data import PokemonData
 from scripts.type import Type
+import time
+import random
 
 def get_types(subtext: str) -> Tuple[Type, Optional[Type]]:
     split = subtext.split('/')
@@ -15,22 +18,31 @@ def get_types(subtext: str) -> Tuple[Type, Optional[Type]]:
     type_2 = Type(re.sub(r'[^a-zA-Z0-9]', '',split[1]).lower()) if len(split) > 1 else None
     return (type_1, type_2)
 
+def build_local_pokedex(subtext: str) -> Dict[str, int]:
+    local_dict: Dict[str, int] = {}
 
-def get_pokemon_data(start_num: int, stop_num: int) -> Dict[str, int]:
+    split_up = subtext.split(')')
+    for local_data in split_up:
+        if '(' not in local_data:
+            continue
+        number, game = local_data.split('(')
+        number = int(re.sub(r'[^a-zA-Z0-9]', '', number))
+        local_dict[game] = number
+
+    return local_dict
+
+def get_pokemon_data(start_num: int, stop_num: int) -> Dict[int, int]:
     # Maps pokemon name to PokemonData construct.
-    pokemon_dict: Dict[str, PokemonData] = {}
+    pokemon_dict: Dict[int, PokemonData] = {}
 
     base_url = 'https://pokemondb.net/pokedex/'
 
-    for natdex_num in range(start_num, stop_num+1):
+    for natdex_num in tqdm(range(start_num, stop_num+1)):
         # Get the HTML context for this Pokemon.
         query_url = base_url + str(natdex_num)
         response = requests.get(query_url)
         soup = BeautifulSoup(response.text, "html.parser")
         text = soup.get_text(separator="\n", strip=True)
-        print(text)
-        print(type(text))
-
 
         # Get Pokemon Name
         # Find the first space. The pokemon's name is everything before that.
@@ -38,23 +50,33 @@ def get_pokemon_data(start_num: int, stop_num: int) -> Dict[str, int]:
         pokemon_name = text[:space_idx]
 
         # Get Pokemon Type(s)
-        preceding_idx = text.find('is a') + 4
+        preceding_idx = text.find('is a') + len('is a')
         succeeding_idx = text.find('type Pokémon')
         enveloping_text = text[preceding_idx:succeeding_idx]
         type_1, type_2 = get_types(enveloping_text)
 
-        print(pokemon_name)
-        print([type_1, type_2])
+        # Build local pokedex.
+        # Get nat-dex nums
+        preceding_idx = text.find('National №') + len('National №')
+        natdex_num = int(text[preceding_idx: preceding_idx + 5])
 
-        #TODO: Build local pokedex.
+        preceding_idx = text.find('Local №') + len('Local №')
+        succeeding_idx = text.find('Training')
+        local_pokedex = build_local_pokedex(text[preceding_idx:succeeding_idx])
 
+        #Construct PokemonData and add to dict.
+        pokemon_data = PokemonData(name = pokemon_name, nat_dex_num= natdex_num, dex_num=local_pokedex, type_1=type_1, type_2=type_2)
+        pokemon_dict[natdex_num] = pokemon_data.to_dict()
 
-        #TODO: Construct PokemonData and add to dict.
-
+        # Sleep for a respectful amount of time.
+        time.sleep(random.uniform(2.1, 3))
 
     return pokemon_dict
 
 
 if __name__ == '__main__': 
-    print(os.getcwd())
-    pokemon_dat = get_pokemon_data(1, 1)
+    pokemon_dat = get_pokemon_data(1, 6)
+
+    dump_path = 'assets\pokemon_data\pokemon.yaml'
+    with open(dump_path, 'w') as file:
+        yaml.dump(pokemon_dat, file, default_flow_style=False)
--- scripts/pokemon_data.py
+++ scripts/pokemon_data.py
@@ -1,5 +1,5 @@
 from dataclasses import dataclass 
-from typing import Dict, Optional
+from typing import Dict, Optional, Union
 from scripts.type import Type
 
 
@@ -7,12 +7,24 @@ from scripts.type import Type
 class PokemonData: 
     """
     name: Pokemon's name.
+    nat_dex_num: Pokemon's national pokedex number.
     dex_num: Dict mapping Pokemon to its dex number.
     type_1: Pokemon's first type
     type_2: Pokemon's second type, if it has one.
     """
     name: str
+    nat_dex_num: int
+
     dex_num: Dict[str, int]
     
     type_1: Type
     type_2: Optional[Type]
+
+    def to_dict(self) -> Dict[str, Union[str, Dict[str, int], int]]:
+        return {
+            'name': self.name,
+            'nat_dex_num': self.nat_dex_num,
+            'dex_num': self.dex_num,
+            'type_1': self.type_1.value,
+            'type_2': self.type_2.value if self.type_2 is not None else None,
+        }
