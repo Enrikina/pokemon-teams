e28f525a1730c1550e1a8d5e0d06cefe65c5b8f7
enrikina <enrikina98@gmail.com>
2025-11-08 11:39:47 -0500

    finish the rby pokedex

--- scripts/make_local_pokedex.py
+++ scripts/make_local_pokedex.py
@@ -6,17 +6,21 @@ def make_local_pokedex(game: str) -> None:
         national_dex = yaml.safe_load(file)  # Returns a dict
 
     local_pokedex: Dict[int, str] = {}
+    counter = 0
 
     for val in national_dex.values():
         local_dex_nums = val['dex_num']
         if (local_dex:= local_dex_nums.get(game)) is not None:
             local_pokedex[local_dex] = val['name']
+        elif (local_dex_2:= local_dex_nums.get('Yellow/Red/Blue')) is not None:
+            local_pokedex[local_dex_2] = val['name']
 
-    dump_path = 'assets\pokemon_data\local_dexes\\hgss.yaml' 
+    
+    dump_path = 'assets\pokemon_data\local_dexes\\rby.yaml' 
     with open(dump_path, 'w') as file:
         yaml.dump(local_pokedex, file, default_flow_style=False)
 
 
 
 if __name__ == '__main__':
-    make_local_pokedex('HeartGold/SoulSilver')
+    make_local_pokedex('Red/Blue/Yellow')
